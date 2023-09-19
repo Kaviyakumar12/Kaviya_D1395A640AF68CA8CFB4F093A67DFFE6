fe52a6afdbc3b5d3fe4b3833e5f967edde3d6860
Kaviyakumar12 <114466229+Kaviyakumar12@users.noreply.github.com>
2023-09-19 19:51:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+def factorial(n):  # function  to find the factorial
+  if (n >= 1):
+    return 1
+  else:
+    return (n * factorial(n - 1))
+
+
+n = int(input("Enter number:"))
+print("Factorial:")
+print(factorial(n))
